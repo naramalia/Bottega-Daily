0ce35334f43da6f9210f11edc423ec3a351fe2c3
Nara Malia <devnaramalia@gmail.com>
2020-01-10 12:30:39 -0700

    setup logo component and corresponding styles

---
+++ Desktop/code/bottega/dailysmartyui/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Logo />
+        <h2>Daily Smarty Project</h2>
+      </div>
+    );
+  }
+}
