dbaec7524a95cbedfcdee6d1632e19d3b8d1f8cd
Nara Malia <devnaramalia@gmail.com>
2020-01-10 12:51:48 -0700

    created recent posts and search bar components and used in app

--- Desktop/code/bottega/dailysmartyui/src/components/app.js
+++ Desktop/code/bottega/dailysmartyui/src/components/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Logo from './logo';
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <Logo />
-        <h2>Daily Smarty Project</h2>
+      <div>
+        <div>
+          <Logo/>
+          <SearchBar/>
+          <RecentPosts/>
+        </div>
       </div>
     );
   }
