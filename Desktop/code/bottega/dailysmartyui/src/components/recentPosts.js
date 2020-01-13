0e6364431378acac36d6e6715867f1ee39d49d2e
Nara Malia <devnaramalia@gmail.com>
2020-01-13 14:15:44 -0700

    modified Logo component to take in a custom size via props

--- Desktop/code/bottega/dailysmartyui/src/components/recentPosts.js
+++ Desktop/code/bottega/dailysmartyui/src/components/recentPosts.js
@@ -4,29 +4,44 @@ import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
+import Post from './post';
+
 class RecentPosts extends Component {
 
     componentDidMount() {
         this.props.fetchRecentPosts();
     }
     
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((posts, index) => {
+            if (index < 3) {
+                return (
+                    <Post {...posts} key={index} />
+                )
+            }
+        })
+        return posts
+    }
 
     render () {
         return (
             <div className="recent-posts">
                 <div className="recent-posts__wrapper">
-                    <div className="recent-posts__heading">
+                    <div className="recent-posts__heading">Recent Posts</div>
                         <ul className="recent-posts__posts">
-                            <li>recent post 0</li>
-                            <li>recent post 1</li>
-                            <li>recent post 2</li>
+                            {this.renderPosts()}
                         </ul>
-                    </div>
                 </div>
 
             </div>
-        );
+        )
     }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
