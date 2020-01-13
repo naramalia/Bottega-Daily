4c3234d5325279d26135b50d8e1cf771af402c06
Nara Malia <devnaramalia@gmail.com>
2020-01-13 14:37:51 -0700

    modified searchBar component to take onSubmit function via props

--- Desktop/code/bottega/dailysmartyui/src/components/results.js
+++ Desktop/code/bottega/dailysmartyui/src/components/results.js
@@ -3,12 +3,17 @@ import Logo from './logo';
 import SearchBar from './searchBar';
 
 class Results extends Component {
+    
+    handleSearchBarSubmit(query) {
+        console.log(query);
+    }
+    
     render () {
         return (
             <div>
 
                 <Logo size={55}/>
-                <SearchBar />
+                <SearchBar onSubmit ={(query) => this.handleSearchBarSubmit} />
             </div>
         )
     }
