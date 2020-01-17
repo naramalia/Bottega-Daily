a51bc60f11472ea1a91b6c6776a81e603def47b1
Nara Malia <devnaramalia@gmail.com>
2020-01-17 09:23:30 -0700

    added react-animate-height npm package and used it to add hover animations to the post links

--- Desktop/code/bottega/dailysmartyui/src/components/post.js
+++ Desktop/code/bottega/dailysmartyui/src/components/post.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+    
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            height: 0
+        }
+    }
     renderTopics() {
         let topics = this.props.associated_topics.map((topic, index) => {
             return <span className="post-topic" key={index}>{topic}</span>
@@ -28,13 +36,13 @@ class Post extends Component {
             return (
             <li className="recent-post">
                 <div className="recent-post__title">
-                    {this.props.title}    
+                    {this.props.title}
                 </div>
                 <div className="recent-post__topics">
                     {this.renderTopics()}
                 </div>
             </li>
-        )
+            )
         } else if(this.props.type == 'result') {
             return (
                 <li className="result-post">
@@ -42,11 +50,20 @@ class Post extends Component {
                         {this.renderTopics()}
                     </div>
                     <div className="result-post__title"> 
-                        {this.props.title}
+                        <a href={this.props.url_for_post}
+                            onMouseEnter={() => this.setState({ height: 70 })}
+                            onMouseLeave={() => this.setState({ height: 0 })}
+                            
+                        >{this.props.title}</a>
                     </div>
+                    <AnimateHeight
+                        duration={ 500 }
+                        height={ this.state.height }
+                    >
                     <div class="result-post__links">
                         {this.renderLinks()}
                     </div>
+                    </AnimateHeight>
                 </li>
             )
         }
